8236aa973f4410ab019f91e1af3671659c40e018
Vidarsskjalf <secure.havig@gmail.com>
2025-08-07 21:07:40 -0700

    Updated script sources

--- install.sh
+++ install.sh
@@ -9,9 +9,17 @@ source $ESSENTIA_INSTALL/preload/dnf.sh
 source $ESSENTIA_INSTALL/preload/flatpak.sh
 
 # Desktop
-source $ESSENTIA_INSTALL/desktop/hyprland.sh
 source $ESSENTIA_INSTALL/desktop/bluetooth.sh
+source $ESSENTIA_INSTALL/desktop/desktop.sh
+source $ESSENTIA_INSTALL/desktop/fonts.sh
+source $ESSENTIA_INSTALL/desktop/hyprland.sh
+source $ESSENTIA_INSTALL/desktop/printer.sh
 
 # Engineering
-source $ESSENTIA_INSTALL/engineering/terminal.sh
 source $ESSENTIA_INSTALL/engineering/docker.sh
+source $ESSENTIA_INSTALL/engineering/engineering.sh
+source $ESSENTIA_INSTALL/engineering/terminal.sh
+
+# Apps
+source $ESSENTIA_INSTALL/apps/mimetypes.sh
+source $ESSENTIA_INSTALL/apps/webapps.sh
