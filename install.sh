9f9370c87a74523f19accc4f8a7bbf77afcdbe7f
Vidarsskjalf <secure.havig@gmail.com>
2025-08-07 18:43:04 -0700

    Created install.sh install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+bash install/preload/dnf.sh
+bash install/preload/flatpak.sh
+
+bash install/desktop/hyprland.sh
+bash install/desktop/bluetooth.sh
+
+bash install/engineering/terminal.sh
+bash install/engineering/docker.sh
