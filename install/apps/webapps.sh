a5833e057310b336a62766eb53c423909fb0ba00
Vidarsskjalf <secure.havig@gmail.com>
2025-08-07 21:03:19 -0700

    Added webapps

---
+++ install/apps/webapps.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+  ~/.local/share/essentia/bin/essentia-webapp-install "WhatsApp" https://web.whatsapp.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/whatsapp.png
+  ~/.local/share/essentia/bin/essentia-webapp-install "Google Messages" https://messages.google.com/web/conversations https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/google-messages.png
+  ~/.local/share/essentia/bin/essentia-webapp-install "ChatGPT" https://chatgpt.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/chatgpt.png
+  ~/.local/share/essentia/bin/essentia-webapp-install "YouTube" https://youtube.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/youtube.png
+  ~/.local/share/essentia/bin/essentia-webapp-install "GitHub" https://github.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/github-light.png
+  ~/.local/share/essentia/bin/essentia-webapp-install "X" https://x.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/x-light.png
