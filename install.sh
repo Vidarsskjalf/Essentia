c3805e9d82eed1cbe0b79591ef230237e9959508
Vidarsskjalf <secure.havig@gmail.com>
2025-08-07 18:44:10 -0700

    Added comments

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Preload
 bash install/preload/dnf.sh
 bash install/preload/flatpak.sh
 
+# Desktop
 bash install/desktop/hyprland.sh
 bash install/desktop/bluetooth.sh
 
+# Engineering
 bash install/engineering/terminal.sh
 bash install/engineering/docker.sh
